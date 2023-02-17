e4a9b11a1c7aa8e005b56dee6625fb9582a590a3
fschwanck <fschwanck@gmail.com>
2023-02-17 16:56:43 -0300

    initial commit

---
+++ src/client/cifar.py
@@ -0,0 +1,31 @@
+
+from collections import OrderedDict
+from typing import Tuple, Dict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+
+import flwr as fl
+
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        params_dict = zip(net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        train(net, trainloader, epochs=1)
+        return self.get_parameters(config={}), num_examples["trainset"], {}
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = test(net, testloader)
+        return float(loss), num_examples["testset"], {"accuracy": float(accuracy)}
